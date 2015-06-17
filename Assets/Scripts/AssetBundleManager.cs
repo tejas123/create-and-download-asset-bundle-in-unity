7b6c90d9c2d0010267bf2d3872441ef76cc3dee1
Bharat <bharatappguru@gmail.com>
2015-06-17 18:24:20 +0530

    Create and Download Asset Bundle in Unity Demo

---
+++ Assets/Scripts/AssetBundleManager.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+static public class AssetBundleManager {
+
+    // A dictionary to hold the AssetBundle references
+    static private Dictionary<string, AssetBundleRef> dictAssetBundleRefs;
+    static AssetBundleManager()
+    {
+        dictAssetBundleRefs = new Dictionary<string, AssetBundleRef>();
+    }
+    // Class with the AssetBundle reference, url and version
+    private class AssetBundleRef
+    {
+        public AssetBundle assetBundle = null;
+        public int version;
+        public string url;
+        public AssetBundleRef(string strUrlIn, int intVersionIn)
+        {
+            url = strUrlIn;
+            version = intVersionIn;
+        }
+    };
+    // Get an AssetBundle
+    public static AssetBundle getAssetBundle(string url, int version)
+    {
+        string keyName = url + version.ToString();
+        AssetBundleRef abRef;
+        if (dictAssetBundleRefs.TryGetValue(keyName, out abRef))
+            return abRef.assetBundle;
+        else
+            return null;
+    }
+    // Download an AssetBundle
+    public static IEnumerator downloadAssetBundle(string url, int version)
+    {
+        AssetLoader.Instance.SetInfoText("Downloading .... ");
+        string keyName = url + version.ToString();
+        using (WWW www = WWW.LoadFromCacheOrDownload(url, version))
+        {
+            yield return www;
+            if (www.error != null)
+            {
+                ///will remove the old file from cashe which cause the problem 
+                AssetBundleManager.Unload(url,version,false);
+                AssetLoader.Instance.SetInfoText("Download error please retry");
+                Debug.Log("Error : "+www.error);
+                throw new Exception("WWW download:" + www.error);
+            }
+            AssetBundleRef abRef = new AssetBundleRef(url, version);
+            abRef.assetBundle = www.assetBundle;
+            
+            if (!dictAssetBundleRefs.ContainsKey(keyName))
+            {
+                dictAssetBundleRefs.Add(keyName, abRef);
+            }
+            else
+            {
+                AssetLoader.Instance.SetInfoText("Asset is unloading plz wait");
+                Debug.Log("This is Just Test that how we can unload asset which is in cache");
+                AssetBundleManager.Unload(url, version, false);
+            }
+        }
+    }
+    // Unload an AssetBundle
+    public static void Unload(string url, int version, bool allObjects)
+    {
+        string keyName = url + version.ToString();
+        AssetBundleRef abRef;
+        if (dictAssetBundleRefs.TryGetValue(keyName, out abRef))
+        {
+            abRef.assetBundle.Unload(allObjects);
+            abRef.assetBundle = null;
+            dictAssetBundleRefs.Remove(keyName);
+        }
+    }
+}
---
+++ Assets/Scripts/AssetLoader.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AssetLoader : MonoBehaviour {
+
+    public static AssetLoader Instance;
+
+    public string url;
+    public int version = 1;
+    public string AssetName;
+
+    public Text infoText;
+
+    AssetBundle bundle;
+
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    void OnDisable()
+    {
+        //AssetBundleManager.Unload(url, version);
+    }
+
+
+    public void DownloadAsset()
+    {
+        // bundle = AssetBundleManager.getAssetBundle (url, version);
+        //   Debug.Log(bundle);
+
+        if (!bundle)
+            StartCoroutine(DownloadAssetBundle());
+    }
+
+    IEnumerator DownloadAssetBundle()
+    {
+        yield return StartCoroutine(AssetBundleManager.downloadAssetBundle(url, version));
+        
+        bundle = AssetBundleManager.getAssetBundle(url, version);
+
+        if (bundle != null)
+            AssetLoader.Instance.SetInfoText("Download Success.... ");
+        else
+            AssetLoader.Instance.SetInfoText("Download error please retry");
+
+        GameObject obj = Instantiate(bundle.LoadAsset("ExampleObject"),Vector3.zero,Quaternion.identity) as GameObject;
+        // Unload the AssetBundles compressed contents to conserve memory
+        //Debug.Log(obj);
+        bundle.Unload(false);
+
+    }
+
+
+    public void SetInfoText(string text)
+    {
+        infoText.text = text;
+    }
+}
